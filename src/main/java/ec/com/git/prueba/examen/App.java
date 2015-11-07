6bd17973cbb5c581a3cd3ce4b88cc841e7e31393
Cynthia Escobar <cescobar@kruger.com.ec>
2015-11-07 08:19:44 -0500

    Initial commit

---
+++ src/main/java/ec/com/git/prueba/examen/App.java
@@ -0,0 +1,13 @@
+package ec.com.git.prueba.examen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
